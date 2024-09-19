951243570148b0d580e854779e41287ffea73691
liankhek <liankhek@gmail.com>
2024-09-19 00:04:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -318,7 +318,7 @@ app.use((err, req, res, next) => {
 });
 
 // listen on port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
  console.log('Listening on Port ' + port);
 });
