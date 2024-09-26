ea54f20764fabaac2c8b656c5434bc978bc99cd4
liankhek <liankhek@gmail.com>
2024-09-26 17:24:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,17 +26,7 @@ mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifie
 
 //app.use(cors());
 const cors = require('cors');
-const allowedOrigins = ['http://localhost:1234', 'https://da-flix-1a4fa4a29dcc.herokuapp.com'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
 
 
 app.use(express.static('public')); // Get documentation file
